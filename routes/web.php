7746bdcf6e112b9d53133f9aadd17f137bca492f
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-31 17:51:03 +0200

    base

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
-use App\Http\Controllers\AboutUsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,3 @@ use App\Http\Controllers\AboutUsController;
 */
 
 Route::get('/',[IndexController::class,'index'])->name('Home');
-Route::get('/chi_siamo',[AboutUsController::class,'about_us'])->name('About_us');
